d8fd4e35d005b86e4f26c608f8ae5f082faf6fd4
ostenilneto <ostenilneto@hotmail.com>
2024-03-05 13:40:16 -0300

    Update Auth.php

--- models/Auth.php
+++ models/Auth.php
@@ -4,16 +4,17 @@ require_once 'dao/UserDaoMysql.php';
 class Auth {
     private $pdo;
     private $base;
+    private $dao;
 
     public function __construct(PDO $pdo, $base) {
         $this->pdo = $pdo;
         $this->base = $base;
+        $this->dao = new UserDaoMysql($this->pdo);
     }
     public function checkToken() {
         if(!empty($_SESSION['token'])) {
             $token = $_SESSION['token'];
-            $userDao = new UserDaoMysql($this->pdo);
-            $user = $userDao->findByToken($token);
+            $user = $this->dao->findByToken($token);
             if($user) {
                 return $user;
             }
@@ -23,14 +24,13 @@ class Auth {
     }
 
     public function validateLogin($email, $password) {
-        $userDao = new UserDaoMysql($this->pdo);
-        $user = $userDao->findByEmail($email);
+        $user = $this->dao->findByEmail($email);
         if($user) {
             if(password_verify($password, $user->password)) {
                 $token = md5(time().rand(0, 9999));
                 $_SESSION['token'] = $token;
                 $user->token = $token;
-                $userDao->update($user);
+                $this->dao->update($user);
 
                 return true;
             }
@@ -40,10 +40,9 @@ class Auth {
     }
 
     public function emailExists($email) {
-        $userDao = new UserDaoMysql($this->pdo);
-        return $userDao->findByEmail($email) ? true : false;
+        return $this->dao->findByEmail($email) ? true : false;
         /* mesma coisa que:
-        if($userDao->findByEmail($email)) {
+        if($this->dao->findByEmail($email)) {
             return true;
         } else {
             return false;
@@ -51,7 +50,6 @@ class Auth {
     }
 
     public function registerUser($name, $email, $password, $birthdate) {
-        $userDao = new UserDaoMysql($this->pdo);
 
         $hash = password_hash($password, PASSWORD_DEFAULT);
         $token = md5(time().rand(0, 9999));
@@ -63,7 +61,7 @@ class Auth {
         $newUser->birthdate = $birthdate;
         $newUser->token = $token;
 
-        $userDao->insert($newUser);
+        $this->dao->insert($newUser);
 
         $_SESSION['token'] = $token;
     }
