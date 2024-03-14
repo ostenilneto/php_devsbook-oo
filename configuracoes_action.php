8f3dd59b59196ed81ea3e441118aafc235551b1a
ostenilneto <ostenilneto@hotmail.com>
2024-03-14 17:58:53 -0300

    Configurações

--- configuracoes_action.php
+++ configuracoes_action.php
@@ -59,6 +59,26 @@ if($name && $email) {
         }
     }
 
+    // AVATAR
+    if(isset($_FILES['avatar']) && !empty($_FILES['avatar']['tmp_name'])) {
+        $newAvatar = $_FILES['avatar'];
+
+        if(in_array($newAvatar['type'], ['image/jpeg','image/jpg','image/png'])) {
+            $avatarWidth = 200;
+            $avatarHeight = 200;
+
+            list($widthOrig. $heightOrig) = getimagesize($newAvatar['tmp_name']);
+            $ratio = $widthOrig / $heightOrig;
+            $newWidht = $avatarWidth;
+            $newHeight = $newWidht / $ratio;
+
+            if($newHeight < $avatarHeight) {
+                $newHeight = $avatarHeight;
+                $newWidht = $newHeight * $ratio;
+            }
+        }
+    }
+    exit;
     $userDao->update($userInfo);
 }
 
