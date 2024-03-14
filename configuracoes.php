b99add7f2d9d63787f2e010a3bedbd9d6add822e
ostenilneto <ostenilneto@hotmail.com>
2024-03-14 16:00:42 -0300

    Configurações

--- configuracoes.php
+++ configuracoes.php
@@ -18,32 +18,34 @@ require 'partials/menu.php';
     <form method="POST" class="config-form" enctype="multipart/form-data" action="configuracoes_action.php">
         <label>
             Novo Avatar:<br/>
-            <input type="file" name="avatar" />
+            <input type="file" name="avatar" /><br/>
+            <img class="mini" src="<?=$base;?>/media/avatars/<?=$userInfo->avatar;?>" />
         </label>
         <label>
             Nova Capa:<br/>
-            <input type="file" name="cover" />
+            <input type="file" name="cover" /><br/>
+            <img class="mini" src="<?=$base;?>/media/covers/<?=$userInfo->cover;?>" />
         </label>
         <hr/>
         <label>
             Nome Completo:<br/>
-            <input type="text" name="name" />
+            <input type="text" name="name" value="<?=$userInfo->name;?>" />
         </label>
         <label>
             E-mail:<br/>
-            <input type="email" name="email" />
+            <input type="email" name="email" value="<?=$userInfo->email;?>" />
         </label>
         <label>
             Data de Nascimento:<br/>
-            <input type="text" name="birthdate" />
+            <input type="text" id="birthdate" name="birthdate" value="<?=date('d/m/Y', strtotime($userInfo->birthdate));?>" />
         </label>
         <label>
             Cidade:<br/>
-            <input type="text" name="city" />
+            <input type="text" name="city" value="<?=$userInfo->city;?>" />
         </label>
         <label>
             Trabalho:<br/>
-            <input type="text" name="work" />
+            <input type="text" name="work" value="<?=$userInfo->work;?>" />
         </label>
         <hr/>
         <label>
@@ -57,6 +59,13 @@ require 'partials/menu.php';
         <button class="button">Salvar</button>        
     </form>
 </section>
+<script src="https://unpkg.com/imask"></script>
+<script>
+    IMask(
+        document.getElementById("birthdate"),
+        {mask:'00/00/0000'}
+    );
+</script>
 <?php
 require 'partials/footer.php';
 ?>
