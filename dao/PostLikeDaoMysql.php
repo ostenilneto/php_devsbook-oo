fb4a110f0f1c6288ee249ab7d87f58c80b58d5e9
ostenilneto <ostenilneto@hotmail.com>
2024-03-18 16:56:38 -0300

    Like

--- dao/PostLikeDaoMysql.php
+++ dao/PostLikeDaoMysql.php
@@ -9,32 +9,49 @@ class PostLikeDaoMysql implements PostLikeDAO {
     }
 
     public function getLikeCount($id_post) {
-        $sql = $this->pdo->prepare("SELECT COUNT(*) as c FROM postlikes 
-            WHERE id_post = :id_post");
-            $sql->bindValue(':id_post', $id_post);
-            $sql->execute();
+        $sql = $this->pdo->prepare("SELECT COUNT(*) as c FROM postlikes
+        WHERE id_post = :id_post");
+
+        $sql->bindValue(':id_post', $id_post);
+        $sql->execute();
 
         $data = $sql->fetch();
         return $data['c'];
-
     }
 
     public function isLiked($id_post, $id_user) {
-        $sql = $this->pdo->prepare("SELECT * FROM postlikes 
-            WHERE id_post = :id_post AND id_user = :id_user");
-            $sql->bindValue(':id_post', $id_post);
-            $sql->bindValue(':id_user', $id_user);
-            $sql->execute();
+        $sql = $this->pdo->prepare("SELECT * FROM postlikes
+        WHERE id_post = :id_post AND id_user = :id_user");
 
-            if($sql->rowCount() > 0) {
-                return true;
-            } else {
-                return false;
-            }
+        $sql->bindValue(':id_post', $id_post);
+        $sql->bindValue(':id_user', $id_user);
+        $sql->execute();
 
+        if($sql->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function likeToggle($id_post, $id_user) {
+        if($this->isLiked($id_post, $id_user)) {
+            $sql = $this->pdo->prepare("DELETE FROM postlikes
+            WHERE id_post = :id_post AND id_user = :id_user");
+        } else {
+            $sql = $this->pdo->prepare("INSERT INTO postlikes
+            (id_post, id_user, created_at) VALUES 
+            (:id_post, :id_user, NOW())");
+        }
 
+        $sql->bindValue(':id_post', $id_post);
+        $sql->bindValue(':id_user', $id_user);
+        $sql->execute();
+    }
+
+    public function deleteFromPost($id_post) {
+        $sql = $this->pdo->prepare("DELETE FROM postlikes WHERE id_post = :id_post");
+        $sql->bindValue(':id_post', $id_post);
+        $sql->execute();
     }
 }
