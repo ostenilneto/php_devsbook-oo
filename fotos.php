4da4af5d64e836a1e2f7422fd187cf90545c87d7
ostenilneto <ostenilneto@hotmail.com>
2024-03-20 18:11:34 -0300

    Upload Photos

--- fotos.php
+++ fotos.php
@@ -27,14 +27,11 @@ if(!$user) {
     header("Location: ".$base);
     exit;
 }
-
-//calculo idade do Usuario
 $dateFrom = new DateTime($user->birthdate);
 $dateTo = new DateTime('today');
 $user->ageYears = $dateFrom->diff($dateTo)->y;
 
-// Verificar se eu SIGO este Usuário
-
+// Verificar se eu SIGO este usuário
 
 require 'partials/header.php';
 require 'partials/menu.php';
@@ -53,7 +50,7 @@ require 'partials/menu.php';
                     <div class="profile-info-name-text"><?=$user->name;?></div>
                     <?php if(!empty($user->city)): ?>
                         <div class="profile-info-location"><?=$user->city;?></div>
-                    <?php endif ;?>
+                    <?php endif; ?>
                 </div>
                 <div class="profile-info-data row">
                     <div class="profile-info-item m-width-20">
@@ -75,31 +72,45 @@ require 'partials/menu.php';
 </div>
 
 <div class="row">
-    <div class="column">
-        <div class="box">
-            <div class="box-body">
-                <div class="full-user-photos">
-                
-                    <?php foreach($user->photos as $key => $item): ?>
-                        <div class="user-photo-item">
-                            <a href="#modal-<?=$key;?>" rel="modal:open">
-                                <img src="<?=$base;?>/media/uploads/<?=$item->body;?>" />
-                            </a>
-                            <div id="modal-<?=$key;?>" style="display:none">
-                                <img src="<?=$base;?>/media/uploads/<?=$item->body;?>" />
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
 
-                    <?php if(count($user->photos) === 0): ?>
-                        Não há fotos deste usuário.
-                    <?php endif; ?>
-                </div>
+<div class="column">
+                    
+    <div class="box">
+        <div class="box-body">
+
+            <div class="full-user-photos">
+
+                <?php foreach($user->photos as $key => $item): ?>
+                    <div class="user-photo-item">
+                        <a href="#modal-<?=$key;?>" data-modal-open>
+                            <img src="<?=$base;?>/media/uploads/<?=$item->body;?>" />
+                        </a>
+                        <div id="modal-<?=$key;?>" style="display:none">
+                            <img src="<?=$base;?>/media/uploads/<?=$item->body;?>" />
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+
+                <?php if(count($user->photos) === 0): ?>
+                    Não há fotos deste usuário.
+                <?php endif; ?>
+
             </div>
+            
+
         </div>
-    </div> 
+    </div>
+
 </div>
+    
+</div>
+
 </section>
+<script>
+window.onload = function() {
+    var modal = new VanillaModal.default();
+};
+</script>
 <?php
 require 'partials/footer.php';
 ?>
